c4c5d0511f719d732b2c5d64402ba57f141a1a11
daniela745 <73662415+daniela745@users.noreply.github.com>
2022-05-06 11:16:15 +0300

    Added my project

---
+++ routes.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/",function(res,res){
+   // console.log("Hello I'm on the start page");
+    res.render("index");
+})
+
+module.exports = router;
