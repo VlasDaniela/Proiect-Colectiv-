b6dad9707435405e8adb784c03e8ae0abb7353d4
VlasDaniela <95177537+VlasDaniela@users.noreply.github.com>
2022-05-06 12:11:10 +0300

    Sign in
    new

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/",function(res,res){
+router.get('/',function(res,res){
    // console.log("Hello I'm on the start page");
-    res.render("index");
+    res.render('index');
 })
-router.get("/views/register",function(res,res){
+router.get('/register',function(res,res){
     // console.log("Hello I'm on the start page");
-     res.render("register");
+     res.render('views/register.ejs');
  })
 
 module.exports = router;
