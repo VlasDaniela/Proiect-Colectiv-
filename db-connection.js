8406c95239436276bbbb461f5dad24009d86fe26
Melinda <73847391+SomodiMelinda00@users.noreply.github.com>
2022-05-07 20:55:30 +0300

    Create db-connection.js

---
+++ db-connection.js
@@ -0,0 +1,11 @@
+var mysql = require("mysql")
+
+const con =
+    mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "taskapp",
+    })
+
+module.exports = con;
