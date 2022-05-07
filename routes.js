71075169e9f6741740e296b2f46d85708f110821
VlasDaniela <95177537+VlasDaniela@users.noreply.github.com>
2022-05-07 19:26:20 +0300

    manager

--- routes.js
+++ routes.js
@@ -18,5 +18,22 @@ router.get('/register',function(res,res){
     // console.log("Hello I'm on the start page");
      res.render('home');
  })
+ router.get('/home_manager',function(res,res){
+    // console.log("Hello I'm on the start page");
+     res.render('home_manager');
+ })
+ router.get('/task_form',function(res,res){
+    // console.log("Hello I'm on the start page");
+     res.render('task_form');
+ })
+ router.get('/task_frame',function(res,res){
+    // console.log("Hello I'm on the start page");
+     res.render('task_frame');
+ })
+ router.get('/view_echipa',function(res,res){
+    // console.log("Hello I'm on the start page");
+     res.render('view_echipa');
+ })
+
 
 module.exports = router;
