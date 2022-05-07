b64fbb25c155bae72bf242d472c4e694242716a7
Melinda <73847391+SomodiMelinda00@users.noreply.github.com>
2022-05-07 20:57:12 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -18,5 +18,17 @@ router.get('/register',function(res,res){
     // console.log("Hello I'm on the start page");
      res.render('home');
  })
+ router.post('/register', (req, res) => {
+    const {
+        email, re_pass, name
+    } = req.body;
+    con.query(`INSERT INTO users (nume, idManager, numeUtilizator, parola, parolaHash, email, profil) VALUES (?, 0, ?, ?, ?, ?, "profil")`, [name, name, re_pass, re_pass, email]),
+    (err) => {
+        if (err) {
+            throw err;
+        }
+        return res.json("User created");
+    }
+ });
 
 module.exports = router;
