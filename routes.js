3383ca2f179698cd5d632ad3099be4add1dbc0d2
VlasDaniela <95177537+VlasDaniela@users.noreply.github.com>
2022-05-13 15:22:22 +0300

    Update routes.js
    sg

--- routes.js
+++ routes.js
@@ -4,17 +4,39 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/',function(res,res){
-   // console.log("Hello I'm on the start page");
-    res.render('index');
+	 // console.log("Hello I'm on the start page");
+     res.render('index');
 })
+
 router.get('/register',function(res,res){
     // console.log("Hello I'm on the start page");
      res.render('register');
  })
+
  router.get('/signin',function(res,res){
     // console.log("Hello I'm on the start page");
      res.render('signin');
  })
+
+ router.post('/signin',function(req,res){
+    const {
+        email, pass
+    } = req.body;
+	console.log(results)
+     
+		con.query('SELECT * FROM users WHERE email = ?', [email, pass], function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+				req.session.email = email;
+				res.redirect('/home');
+			} else {
+				res.send('Incorrect Email and/or Password!');
+			}			
+			res.end();
+		});
+	
+});
+
  router.get('/home',function(res,res){
     // console.log("Hello I'm on the start page");
      res.render('home');
@@ -23,7 +45,7 @@ router.get('/register',function(res,res){
     const {
         email, re_pass, name
     } = req.body;
-    con.query(`INSERT INTO users (nume, idManager, numeUtilizator, parola, parolaHash, email, profil) VALUES (?, 0, ?, ?, ?, ?, "profil")`, [name, name, re_pass, re_pass, email]),
+    con.query(INSERT INTO users (nume, idManager, numeUtilizator, parola, parolaHash, email, profil) VALUES (?, 0, ?, ?, ?, ?, "profil"), [name, name, re_pass, re_pass, email]),
     (err) => {
         if (err) {
             throw err;
@@ -31,5 +53,7 @@ router.get('/register',function(res,res){
         return res.json("User created");
     }
  });
+
+ 
 
 module.exports = router;
